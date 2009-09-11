42bc7947cfd2ae6d7faa44e19d46795d5a37b48d
Atsushi Shimono <shimono@bug-ja.org>
2009-09-11 17:16:25 +0000

    bug-ja/trunk: updated to the current CVS 20090912 (as for 3.4.2)
    git-svn-id: https://ssl.jpmoz.net/svn/bug-ja/bugzilla-l10n-ja/trunk@485 3fd43d0e-4793-11dd-8047-fd5b7de829c9

--- Bugzilla/BugMail.pm
+++ Bugzilla/BugMail.pm
@@ -327,11 +327,10 @@ sub Send {
                     $deptext .= $thisdiff;
                 }
                 $lastbug = $depbug;
-                my $urlbase = Bugzilla->params->{"urlbase"};
                 $thisdiff =
                   "\nBug $id depends on bug $depbug, which changed state.\n\n" .
                   "Bug $depbug Summary: $summary\n" .
-                  "${urlbase}show_bug.cgi?id=$depbug\n\n";
+                  correct_urlbase() . "show_bug.cgi?id=$depbug\n\n";
                 $thisdiff .= three_columns("What    ", "Old Value", "New Value");
                 $thisdiff .= ('-' x 76) . "\n";
                 $interestingchange = 0;
@@ -577,21 +576,10 @@ sub sendMail {
             $difftext .= $diff->{'text'};
         }
     }
- 
-    if ($difftext eq "" && $newcomments eq "" && !$isnew) {
+
+    if ($difftext eq "" && !scalar(@$newcomments) && !$isnew) {
       # Whoops, no differences!
       return 0;
-    }
-    
-    # If an attachment was created, then add an URL. (Note: the 'g'lobal
-    # replace should work with comments with multiple attachments.)
-
-    if ( $newcomments =~ /Created an attachment \(/ ) {
-
-        my $showattachurlbase =
-            Bugzilla->params->{'urlbase'} . "attachment.cgi?id=";
-
-        $newcomments =~ s/(Created an attachment \(id=([0-9]+)\))/$1\n --> \(${showattachurlbase}$2\)/g;
     }
 
     my $diffs = $difftext;
@@ -687,9 +675,15 @@ sub get_comments_by_bug {
 
     my $raw = 1; # Do not format comments which are not of type CMT_NORMAL.
     my $comments = Bugzilla::Bug::GetComments($id, "oldest_to_newest", $start, $end, $raw);
+    my $attach_base = correct_urlbase() . 'attachment.cgi?id=';
 
     foreach my $comment (@$comments) {
         $comment->{count} = $count++;
+        # If an attachment was created, then add an URL. (Note: the 'g'lobal
+        # replace should work with comments with multiple attachments.)
+        if ($comment->{body} =~ /Created an attachment \(/) {
+            $comment->{body} =~ s/(Created an attachment \(id=([0-9]+)\))/$1\n --> \($attach_base$2\)/g;
+        }
     }
 
     if (Bugzilla->params->{'insidergroup'}) {
