a9bd1de9abe25198c53e0cd1567d08b150411745
Atsushi Shimono <shimono@bug-ja.org>
2011-02-14 12:26:25 +0000

    bug-ja/3.6: updated to the current (bzr rev. 7234)
    git-svn-id: https://ssl.jpmoz.net/svn/bug-ja/bugzilla-l10n-ja-3.6/trunk@674 3fd43d0e-4793-11dd-8047-fd5b7de829c9

--- mod_perl.pl
+++ mod_perl.pl
@@ -18,6 +18,14 @@
 package Bugzilla::ModPerl;
 
 use strict;
+use warnings;
+
+# This sets up our libpath without having to specify it in the mod_perl
+# configuration.
+use File::Basename;
+use lib dirname(__FILE__);
+use Bugzilla::Constants ();
+use lib Bugzilla::Constants::bz_locations()->{'ext_libpath'};
 
 # If you have an Apache2::Status handler in your Apache configuration,
 # you need to load Apache2::Status *here*, so that any later-loaded modules
@@ -38,7 +46,6 @@ use Template::Config ();
 Template::Config->preload();
 
 use Bugzilla ();
-use Bugzilla::Constants ();
 use Bugzilla::CGI ();
 use Bugzilla::Extension ();
 use Bugzilla::Install::Requirements ();
@@ -99,6 +106,14 @@ my $rl = new ModPerl::RegistryLoader();
 # Bugzilla/ModPerl/ResponseHandler.pm
 $rl->{package} = 'Bugzilla::ModPerl::ResponseHandler';
 my $feature_files = Bugzilla::Install::Requirements::map_files_to_features();
+
+# Prevent "use lib" from doing anything when the .cgi files are compiled.
+# This is important to prevent the current directory from getting into
+# @INC and messing things up. (See bug 630750.)
+no warnings 'redefine';
+local *lib::import = sub {};
+use warnings;
+
 foreach my $file (glob "$cgi_path/*.cgi") {
     my $base_filename = File::Basename::basename($file);
     if (my $feature = $feature_files->{$base_filename}) {
@@ -119,6 +134,14 @@ sub handler : method {
     # $0 is broken under mod_perl before 2.0.2, so we have to set it
     # here explicitly or init_page's shutdownhtml code won't work right.
     $0 = $ENV{'SCRIPT_FILENAME'};
+
+    # Prevent "use lib" from modifying @INC in the case where a .cgi file
+    # is being automatically recompiled by mod_perl when Apache is
+    # running. (This happens if a file changes while Apache is already
+    # running.)
+    no warnings 'redefine';
+    local *lib::import = sub {};
+    use warnings;
 
     Bugzilla::init_page();
     return $class->SUPER::handler(@_);
